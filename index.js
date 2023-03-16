94d30303bd2749bf5a1c606b6ef28a116fdb9283
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-03-16 16:33:21 +0500

    Add a reference to buttons section

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const CHOICES = ["rock", "paper", "scissors"];
+const buttons = document.querySelector(".buttons");
 
 function capitalize(string) {
   return string[0].toUpperCase() + string.slice(1);
